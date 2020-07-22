011e4734eb527439e44c450aeebed388cab556f6
derekSaga <40836760+derekSaga@users.noreply.github.com>
2020-07-22 14:39:24 -0300

    inicio da implementação de token

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api
@@ -10,6 +11,7 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
 api = Api(app)
+JWTManager(app)
 
-from .views import tarefa_view, projeto_view, funcionario_view
-from .models import tarefa_model, projeto_model, funcionario_model
+from .views import tarefa_view, projeto_view, funcionario_view, usuario_view
+from .models import tarefa_model, projeto_model, funcionario_model, usuario_model
--- api/models/usuario_model.py
+++ api/models/usuario_model.py
@@ -8,7 +8,7 @@ class UsuarioModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
     senha = db.Column(db.String(255), nullable=False)
 
     def gen_senha(self):
